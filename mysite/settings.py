9bcf68af8848ed31e25fff482ba97a368096b381
hobin505 <57647941+hobin505@users.noreply.github.com>
2020-02-21 00:26:26 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6%9yok&pkmp5=l7n#yv3-*5fp8n_o=1@5-%-gz#6l1p2e%164b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [hobin505.pythonanywhere.com]
+ALLOWED_HOSTS = ['hobin505.pythonanywhere.com','localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
